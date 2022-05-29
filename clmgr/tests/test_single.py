13c8e25efe1e789d8da7f094cf61f5bb0040594c
Gert-Jan Timmer <gert-jan.timmer@enovationgroup.com>
2022-05-29 18:24:53 +0200

    chore: eol

--- clmgr/tests/test_single.py
+++ clmgr/tests/test_single.py
@@ -8,6 +8,11 @@ test_dir = os.path.dirname(os.path.realpath(__file__))
 
 def test_single_java():
         global test_dir
+
         test_args = ['-c', test_dir + '/config/single.yml', '--file', test_dir + '/input/java/single.java', '--header-length', '120']
+
+        # Run clmgr
         main(test_args)
+
+        # Verify result
         assert filecmp.cmp(test_dir + '/input/java/single.java', test_dir + '/output/java/single.java', shallow=False)
