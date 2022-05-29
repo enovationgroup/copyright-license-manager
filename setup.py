9a66b2295123c8de4cc63031ae48a188316fd4e6
Gert-Jan Timmer <gert-jan.timmer@enovationgroup.com>
2022-05-29 22:03:37 +0200

    chore: set minimal python to 3.8

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ DESCRIPTION = "Copyright License Manager"
 URL = "https://github.com/enovationgroup/copyright-license-manager"
 AUTHOR = "Enovation Group"
 EMAIL = "development@enovationgroup.com"
-REQUIRES_PYTHON = ">3.6"
+REQUIRES_PYTHON = ">3.8"
 LICENSE = "MIT"
 
 cwd = os.path.abspath(os.path.dirname(__file__))
