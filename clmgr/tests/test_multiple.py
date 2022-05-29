54757cbd1e67422eb03b05ab8f9f5f2f0e8949bd
Gert-Jan Timmer <gert-jan.timmer@enovationgroup.com>
2022-05-29 18:36:40 +0200

    test: fix filenames

--- clmgr/tests/test_multiple.py
+++ clmgr/tests/test_multiple.py
@@ -6,12 +6,12 @@ from clmgr.main import main
 
 test_dir = os.path.dirname(os.path.realpath(__file__))
 
-def test_single_java():
+def test_multiple_java():
         global test_dir
 
         # Test arguments
-        test_args = ['-c', test_dir + '/config/multiple.yml', '--file', test_dir + '/input/java/multiple.java', '--header-length', '120']
+        test_args = ['-c', test_dir + '/config/multiple.yml', '--file', test_dir + '/input/java/Multiple.java', '--header-length', '120']
 
         main(test_args)
 
-        assert filecmp.cmp(test_dir + '/input/java/multiple.java', test_dir + '/output/java/multiple.java', shallow=False)
+        assert filecmp.cmp(test_dir + '/input/java/Multiple.java', test_dir + '/output/java/Multiple.java', shallow=False)
--- clmgr/tests/test_single.py
+++ clmgr/tests/test_single.py
@@ -9,10 +9,10 @@ test_dir = os.path.dirname(os.path.realpath(__file__))
 def test_single_java():
         global test_dir
 
-        test_args = ['-c', test_dir + '/config/single.yml', '--file', test_dir + '/input/java/single.java', '--header-length', '120']
+        test_args = ['-c', test_dir + '/config/single.yml', '--file', test_dir + '/input/java/Single.java', '--header-length', '120']
 
         # Run clmgr
         main(test_args)
 
         # Verify result
-        assert filecmp.cmp(test_dir + '/input/java/single.java', test_dir + '/output/java/single.java', shallow=False)
+        assert filecmp.cmp(test_dir + '/input/java/Single.java', test_dir + '/output/java/Single.java', shallow=False)
