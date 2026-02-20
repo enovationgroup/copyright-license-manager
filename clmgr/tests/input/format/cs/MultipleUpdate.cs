388a6ba7cbacf3e55105d0b14e7071876a236e94
Albert-Jan Verhees <albert-jan.verhees@enovationgroup.com>
2026-02-20 10:43:47 +0100

    chore: cleaned up tests

--- clmgr/tests/input/format/cs/MultipleUpdate.cs
+++ clmgr/tests/input/format/cs/MultipleUpdate.cs
@@ -1,7 +1,7 @@
 /*************************************************************************
- * SPDX-FileCopyrightText: Copyright (c) 2014 - 2016 [Mars Hospital - Rotterdam - NL]
- * SPDX-FileCopyrightText: Copyright (c) 2016 - 2018 [Lunar Base - Capelle aan den IJssel - NL]
- * SPDX-FileCopyrightText: Copyright (c) 2018 - 2020 [Enovation Group B.V. - Capelle aan den IJssel - NL]
+ * Copyright (c) 2014 - 2016 [Mars Hospital - Rotterdam - NL]
+ * Copyright (c) 2016 - 2018 [Lunar Base - Capelle aan den IJssel - NL]
+ * Copyright (c) 2018 - 2020 [Enovation Group B.V. - Capelle aan den IJssel - NL]
  *
  */
 using System.Collections.Generic;
--- clmgr/tests/input/format/cs/Remove.cs
+++ clmgr/tests/input/format/cs/Remove.cs
@@ -1,6 +1,6 @@
 /*************************************************************************
- * SPDX-FileCopyrightText: Copyright (c) 2014 - 2016 [Mars Hospital - Rotterdam - NL]
- * SPDX-FileCopyrightText: Copyright (c) 2016 - 2018 [Lunar Base - Capelle aan den IJssel - NL]
+ * Copyright (c) 2014 - 2016 [Mars Hospital - Rotterdam - NL]
+ * Copyright (c) 2016 - 2018 [Lunar Base - Capelle aan den IJssel - NL]
  *
  */
 using System.Collections.Generic;
